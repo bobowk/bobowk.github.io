d3141a4be0bf8ee3c02ad21d95e1d47bf1cd1257
bobowk <60067581+bobowk@users.noreply.github.com>
2024-03-11 01:53:37 +1100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,29 +1,27 @@
-$(window).on("load",function() {
-    function fade(pageLoad) {
-      var windowTop=$(window).scrollTop(), windowBottom=windowTop+$(window).innerHeight();
-      var min=-0.8, max=1, threshold=0.01;
-      
-      $(".fade").each(function() {
-        /* Check the location of each desired element */
-        var objectHeight=$(this).outerHeight(), objectTop=$(this).offset().top, objectBottom=$(this).offset().top+objectHeight;
-        
-        /* Fade element in/out based on its visible percentage */
-        if (objectTop < windowTop) {
-          if (objectBottom > windowTop) {$(this).fadeTo(0,min+((max-min)*((objectBottom-windowTop)/objectHeight)));}
-          else if ($(this).css("opacity")>=min+threshold || pageLoad) {$(this).fadeTo(0,min);}
-        } else if (objectBottom > windowBottom) {
-          if (objectTop < windowBottom) {$(this).fadeTo(0,min+((max-min)*((windowBottom-objectTop)/objectHeight)));}
-          else if ($(this).css("opacity")>=min+threshold || pageLoad) {$(this).fadeTo(0,min);}
-        } else if ($(this).css("opacity")<=max-threshold || pageLoad) {$(this).fadeTo(0,max);}
-      });
-    } fade(true); //fade elements on page-load
-    $(window).scroll(function(){fade(false);}); //fade elements on scroll
-  });
-  $('#nav a').hover(function() {
-    $(this).find('.label').show();
-  }, function() {
-    $(this).find('.label').hide();
-  });
+$(window).on("load", function() {
+  if ($(window).width() > 850) {
+      function fade(pageLoad) {
+          var windowTop = $(window).scrollTop(), windowBottom = windowTop + $(window).innerHeight();
+          var min = -0.8, max = 1, threshold = 0.02;
+
+          $(".fade").each(function() {
+              /* Check the location of each desired element */
+              var objectHeight = $(this).outerHeight(), objectTop = $(this).offset().top, objectBottom = $(this).offset().top + objectHeight;
+
+              /* Fade element in/out based on its visible percentage */
+              if (objectTop < windowTop) {
+                  if (objectBottom > windowTop) { $(this).fadeTo(0, min + ((max - min) * ((objectBottom - windowTop) / objectHeight))); }
+                  else if ($(this).css("opacity") >= min + threshold || pageLoad) { $(this).fadeTo(0, min); }
+              } else if (objectBottom > windowBottom) {
+                  if (objectTop < windowBottom) { $(this).fadeTo(0, min + ((max - min) * ((windowBottom - objectTop) / objectHeight))); }
+                  else if ($(this).css("opacity") >= min + threshold || pageLoad) { $(this).fadeTo(0, min); }
+              } else if ($(this).css("opacity") <= max - threshold || pageLoad) { $(this).fadeTo(0, max); }
+          });
+      }
+      fade(true); //fade elements on page-load
+      $(window).scroll(function() { fade(false); }); //fade elements on scroll
+  }
+});
 
 
   $(document).ready(function($){
